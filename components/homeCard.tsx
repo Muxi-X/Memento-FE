35b482771fb8ddff9eeb569bb06fc715dcde4b02
kazumajj <1990342976@qq.com>
2026-04-09 20:00:22 +0800

    更新homeCard组件

--- components/homeCard.tsx
+++ components/homeCard.tsx
@@ -1,61 +1,70 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Pressable,
-  Image,
-} from "react-native";
-import * as Progress from 'react-native-progress';
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
+import * as Progress from "react-native-progress";
 import ArrowRgiht from "../assets/images/arrow-right.svg";
-import CoverImageNull  from "../assets/images/coverImagenull.svg";
-import { coverImage } from "@/app/api/interface";
+import CoverImage from "../assets/images/baseCover.svg";
+import { Notificover } from "@/app/api/interface";
+import { router } from "expo-router";
 type Props = {
   hasAim: boolean;
   target: number;
   progress: number;
   title: string;
-  cover: coverImage|null;
+  cover?: Notificover | null;
+  keyword_id:string;
 };
 export default function HomeCard(props: Props) {
   return (
     <View style={styles.card}>
-      {      props.cover!==null ?
-      <View style={styles.Imagedata}>
+      {props.cover  ? (
+        <Image
+          source={{ uri: props.cover?.variants.square_small.url }}
+          style={styles.Imagedata}
+        />
+      ) : (
+        <CoverImage></CoverImage>
+      )}
 
-  <Image source={{uri:props.cover.variants.card_4x3.url}}/></View>
-:
-  <View style={[styles.Imagedata ,{backgroundColor:"#EFEFEF",alignItems:"center",justifyContent:"center"
-  }]}>
-    <Text style={{ fontSize: 36, color: "#666666",  }}>?</Text>
-    </View>}
-      
-      
       <Text style={{ fontSize: 18, color: "#3D3D3D", marginLeft: 12 }}>
         {props.title}
       </Text>
-     <View style={{flexDirection: "row",alignItems: "center",position:"absolute",top:33,right:21}}>
-       <Text style={{ fontSize: 12, color: "#666666", marginLeft: 116 }}>
-        {props.progress}张作品
-      </Text>
-      <Pressable
-        style={styles.detailArrow}
-        onPress={() => {
-          console.log("detail");
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          position: "absolute",
+          top: 33,
+          right: 21,
         }}
       >
-        <ArrowRgiht></ArrowRgiht>
-      </Pressable>
-     </View>
+        <Text style={{ fontSize: 12, color: "#666666", marginLeft: 116 }}>
+          {props.progress}张作品
+        </Text>
+        <Pressable
+          style={styles.detailArrow}
+          onPress={() => {
+            router.navigate(
+              {
+                pathname: "/customPage",
+                params: { keyword: props.title,
+                  keyword_id:props.keyword_id
+                 }
+              }
+            )
+          }}
+        >
+          <ArrowRgiht></ArrowRgiht>
+        </Pressable>
+      </View>
       {props.hasAim && (
         <Progress.Bar
           style={styles.progressBar}
           height={8}
           width={100}
-        progress={props.progress/props.target}
-        borderWidth={0}
-        color="#72B6FF"
-        unfilledColor="#EEEEEE"
+          progress={props.progress / props.target}
+          borderWidth={0}
+          color="#72B6FF"
+          unfilledColor="#EEEEEE"
         ></Progress.Bar>
       )}
     </View>
@@ -90,8 +99,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   progressBar: {
-    position:"absolute",
-    top:50,
-    left:79
+    position: "absolute",
+    top: 50,
+    left: 79,
   },
 });
